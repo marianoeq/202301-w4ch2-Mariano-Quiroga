ff57463346d1053c376bea0b363b321651b9518a
mariano quiroga <mariano.eq@gmail.com>
2023-02-08 01:23:56 +0100

    Create test

--- src/components/core/app/App.test.tsx
+++ src/components/core/app/App.test.tsx
@@ -1,8 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import { App } from "./App";
-
+import { Header } from "../../../pages/header/header";
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Header />);
+  const linkElement = screen.getByText(/fill the form/i);
   expect(linkElement).toBeInTheDocument();
 });
